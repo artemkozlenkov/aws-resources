a3b853c3ff1419fbdc7221c6cf305b97ce13df79
artem kozlenkov <artem.kozlenkov@gmail.com>
2021-05-25 00:23:10 +0200

    updates

--- infra/terraform/test/gateway/settings.tf
+++ infra/terraform/test/gateway/settings.tf
@@ -37,7 +37,7 @@ data "aws_vpc" "selected" {
     Name = "cluster"
   }
 }
-data "aws_subnet" "public" {
+data "aws_subnet_ids" "public" {
   filter {
     name   = "tag:Name"
     values = ["public-subnet"]
