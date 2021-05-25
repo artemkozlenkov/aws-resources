627248a7a2a68b30b8e819823a81e093e15d782b
artem kozlenkov <artem.kozlenkov@gmail.com>
2021-05-25 21:11:29 +0200

    add http-80-tcp

--- infra/terraform/test/discourse/main.tf
+++ infra/terraform/test/discourse/main.tf
@@ -60,7 +60,7 @@ module "asg_sg" {
   description = "A security group"
   vpc_id      = data.aws_vpc.cluster.id
 
-  computed_ingress_rules           = ["ssh-tcp","http-80"]
+  computed_ingress_rules           = ["ssh-tcp","http-80-tcp"]
   number_of_computed_ingress_rules = 1
   ingress_cidr_blocks              = ["0.0.0.0/0"]
 
