f00b5e7c14fca7de0d79a4ddb6ec36621c08969e
artem kozlenkov <artem.kozlenkov@gmail.com>
2021-05-27 23:22:23 +0200

    updatess

--- infra/terraform/dev/network/main.tf
+++ infra/terraform/dev/network/main.tf
@@ -57,5 +57,5 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "gw" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = data.aws_subnet_ids.public[0].id
+  subnet_id     = data.aws_subnet_ids.public.ids[0]
 }
